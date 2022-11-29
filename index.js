a4b031a8994fb1a9d300070fd137533e5d3b5336
Rukorio-Vanessa <blwwbl55@gmail.com>
2022-11-29 15:09:50 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const mondayWork = function(activity = "go to the office"){
     return `This Monday, I will ${activity}.`
 }
 
-let wrapAdjective = function(sign="*") {
+const wrapAdjective = function(sign="*") {
     return function(description="a hard worker") {
       return `You are ${sign}${description}${sign}!`
     }
